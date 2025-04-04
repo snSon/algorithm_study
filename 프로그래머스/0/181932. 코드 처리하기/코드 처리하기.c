e22af672eca20a7596bf7c9949010717fa93185b
snSon <126743797+snSon@users.noreply.github.com>
2025-04-04 15:12:22 +0900

    [level 0] Title: 코드 처리하기, Time: 1.46 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* code) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int answer_len = 0;
+    char* answer = (char*)malloc(answer_len + 1);
+    
+    int code_len = strlen(code);
+    int mode = 0;
+    
+    for(int idx = 0; idx < code_len; idx++){
+        if(code[idx] == '1'){
+            mode = mode ^ 1;
+        }
+        else{
+            if(mode == 0){
+                if((idx & 1) == 0){
+                    answer_len++;
+                    answer = (char*)realloc(answer, sizeof(char) * (answer_len + 1)); 
+                    answer[answer_len - 1] = code[idx]; 
+                }
+            }
+            else if (mode == 1){
+                if((idx & 1) != 0){
+                    answer_len++;
+                    answer = (char*)realloc(answer, sizeof(char) * (answer_len + 1)); 
+                    answer[answer_len - 1] = code[idx];                
+                }
+            }
+        }
+    }
+    
+    if(answer_len == 0){
+        return "EMPTY";
+    }
+    answer[answer_len] = '\0';
+    
+    
+    return answer;
+}

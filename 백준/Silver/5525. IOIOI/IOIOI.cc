baf27c27dce3239ad3258f162951bd1d6d3691a6
snSon <126743797+snSon@users.noreply.github.com>
2025-10-24 16:29:01 +0900

    [Silver I] Title: IOIOI, Time: 44 ms, Memory: 4916 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -4,22 +4,22 @@
 int main(){
     int n, m;
     std::string s;
-
     std::cin >> n >> m >> s;
-
-    std::string target = "I";
-    for(int i = 0; i < n; i++){
-        target += "OI";
-    }
 
     int idx = 0, cnt = 0;
     while(true){
-        idx = s.find(target, idx);
-        if(idx == -1){
+        idx = s.find("IOI", idx);
+        if(idx == -1)
             break;
+        
+        int curr_cnt = 0;
+        while(s[idx + 1] == 'O' && s[idx + 2] == 'I'){
+            curr_cnt++;
+            idx += 2;
         }
-        idx++;
-        cnt++;
+        if(curr_cnt >= n){
+            cnt += (curr_cnt + 1 - n);
+        }
     }
 
     std::cout << cnt << std::endl;
